ff0ba439aab4e8e2fc9acb74cbd5a8c1ad36bef2
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-13 14:12:16 +0800

    initial commit

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical
+"Quick Pick" Lottery Ticket Generator
+File: quick_picks.py
+
+Write a program that asks the user how many "quick picks" they wish to generate.
+The program then generates that many lines of output. Each line consists of 6 random numbers between 1 and 45.
+These values should be stored as CONSTANTS.
+
+Each line (quick pick) should not contain any repeated number.
+Each line of numbers should be displayed in sorted (ascending) order.
+Study the formatting below so that numbers align neatly.
+
+Your code should produce output that matches this sample output (except the numbers are random):
+
+How many quick picks? 5
+ 1 12 14 15 30 36
+ 2  5  8 33 38 41
+ 2 12 19 22 29 43
+13 21 28 29 42 43
+ 3  4 10 11 32 44
+"""
+
+import random
