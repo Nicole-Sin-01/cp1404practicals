32b63c2f9e4288e1ed6155424eafced04b868477
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-11-03 23:42:31 +0800

    completed the init, formatting and get)age and is_vintage functions

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -30,3 +30,27 @@ Hint: try using get_age() to simplify the implementation of this method!
 Remember that methods should not take in any data that the object already knows (like age, year, etc.).
 """
 
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance.
+
+        name: string, the name of the guitar
+        year: integer, the year the guitar was made
+        cost: float, the cost of the guitar
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the guitar using {} formatting."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years based on the current year."""
+        current_year = 2024
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, otherwise False."""
+        return self.get_age() >= 50
