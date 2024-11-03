2efe672ed39b60f4ae8bc50370244ecbb963b0cc
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-11-03 23:27:18 +0800

    completed the class, defined and printed the languages in the specified formats

---
+++ prac_06/languages.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical
+
+It should return a string like:
+Python, Dynamic Typing, Reflection=True, First appeared in 1991
+"""
+
+from programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+
+    languages = [python, ruby, visual_basic] # Create list of languages
+
+    print("The dynamically typed languages are:") # Find and print languages with dynamic typing
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+if __name__ == "__main__":
+    main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,38 @@
+"""
+CP1404/CP5632 Practical
+
+Define the following methods:
+
+__init__ - like most init functions, create the fields and set them to the parameters passed in.
+
+is_dynamic() - which returns True/False if the programming language is dynamically typed or not.
+
+[IMPORTANT] Please understand that this function will take no parameters other than self. The data is already inside the object, so you don't need to tell the object its own data.
+
+This function's purpose is to encapsulate the functionality that would make the class more helpful.
+See how the function name starts with is, like isupper(), isnumeric(), etc.?
+So, it returns a Boolean.
+"""
+
+class ProgrammingLanguage:
+    """Represent a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a programming language instance.
+        name: string, reference name for programming language
+        typing: string, typing discipline of the language, Static or Dynamic
+        reflection: string, whether the language supports reflection, Yes or No
+        year: integer, release year of the language
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the language is dynamic, else False."""
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Return a formatted string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
