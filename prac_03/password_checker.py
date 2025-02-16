03983e6afebb98f972ba372fed926a61c1a6d188
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-16 15:38:08 +0800

    Added the validity checker specifics to count for the needed attributes of the password

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -37,3 +37,38 @@ def main():
         print("Invalid password!")
         password = input("> ")
     print(f"Your {len(password)}-character password is valid: {password}")
+
+def is_valid_password(password):
+    """Determine if the provided password is valid."""
+    # If length is wrong, return False
+    if len(password) < MIN_LENGTH or len(password)  > MAX_LENGTH:
+        return False
+
+    number_of_lower = 0
+    number_of_upper = 0
+    number_of_digit = 0
+    number_of_special = 0
+
+    # Count each kind of character (use str methods like isdigit)
+    for character in password:
+        if character.islower():
+            number_of_lower += 1
+        elif character.isupper():
+            number_of_upper += 1
+        elif character.isdigit():
+            number_of_digit += 1
+        elif character in SPECIAL_CHARACTERS:
+            number_of_special += 1
+
+    # If any of the 'normal' counts are zero, return False
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
+
+    # If special characters are required, then check the count of those and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
+        return False
+
+    # if we get here (without returning False), then the password must be valid
+    return True
+
+main()
