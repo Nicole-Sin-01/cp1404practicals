531497777e381fa09ecfaaaa50147b504acf1d2f
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-16 16:22:04 +0800

    Added validity checker , and functions to determine the results and print stars

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -58,5 +58,37 @@ def main():
         else:
             print("Invalid choice. Please try again.")
 
+
+def print_menu():
+    """Display menu."""
+    print("\nMenu:")
+    print("(G)et a valid score")
+    print("(P)rint result")
+    print("(S)how stars")
+    print("(Q)uit")
+
+def determine_result(score):
+    """Determine the result based on the score."""
+    if score >= EXCELLENT_THRESHOLD:
+        return "Excellent"
+    elif score >= PASSABLE_THRESHOLD:
+        return "Passable"
+    else:
+        return "Bad"
+
+def get_valid_score():
+    """Prompt the user for a valid score."""
+    score = float(input(f"Enter a score (between {MINIMUM_SCORE} & {MAXIMUM_SCORE}): "))
+    while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        print("Invalid score. Please try again.")
+        score = float(input(f"Enter a score (between {MINIMUM_SCORE} & {MAXIMUM_SCORE}): "))
+    return score
+
+
+def print_stars(score):
+    """Print stars equal to the score value."""
+    # Change from float to integer so it works
+    print(int(score) * "*")
+
 if __name__ == "__main__":
     main()
