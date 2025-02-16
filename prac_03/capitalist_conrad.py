c8cf27cc71d5ca5425fb174899bda08491c1d5d8
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-16 15:30:23 +0800

    Added code to increase or decrease the stock market prices based on random generation

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -20,3 +20,25 @@ FILENAME = "stock_price_simulator.txt"  # Output file name
 price = INITIAL_PRICE
 day = 0
 
+out_file = open(FILENAME, 'w')
+
+print(f"Starting price: ${price:,.2f}")
+
+while MIN_PRICE <= price <= MAX_PRICE:
+    price_change = 0
+    day += 1
+    # Generate a random integer of 1 or 2; The price increases on a 1, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        # generate a random floating-point number
+        # between negative MAX_DECREASE and 0
+        price_change = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + price_change)
+
+    print(f"On day {day} the price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
