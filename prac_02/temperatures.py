1bd74134389534c9b248ef246eac8e2b1ede75e8
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-16 16:24:46 +0800

    Added main skeleton to handle relevant actions for each choice

---
+++ prac_02/temperatures.py
@@ -0,0 +1,37 @@
+"""
+CP1404 - Practical
+Temperature Conversion Program
+
+def celsius_to_fahrenheit()
+    celsius * 9.0 / 5.0 + 32
+
+def fahrenheit_to_celsius()
+    (fahrenheit - 32) * 5.0 / 9.0
+
+def main()
+    header
+    menu
+        Option 1: Celsius to Fahrenheit
+        Option 2: Fahrenheit to Celsius
+        Invalid option
+"""
+
+def main():
+    """Main function to handle input and temperature conversions."""
+    choice = input("Temperature Converter\n'C': Convert Celsius to Fahrenheit\n'F': Convert Fahrenheit to Celsius\n>>> ").upper()
+    if choice == 'C':
+        celsius = float(input("Enter temperature in Celsius: "))
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        print(f"{celsius}°C is {fahrenheit:.2f}°F.")
+
+    elif choice == 'F':
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+        celsius = fahrenheit_to_celsius(fahrenheit)
+        print(f"{fahrenheit}°F is {celsius:.2f}°C.")
+
+    else:
+        print("Invalid option. Please enter 'C' or 'F'.")
+
+
+if __name__ == "__main__":
+    main()
