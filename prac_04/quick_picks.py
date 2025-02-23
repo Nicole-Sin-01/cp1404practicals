d01d33ca7f89e79ff1400032b759b039293b7445
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-23 10:28:25 +0800

    Added a function to generate each row of numbers randomly, and sort them in ascending order

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,6 +7,10 @@ MAXIMUM_NUMBER = 45
 def main():
     quick_picks_count = get_valid_pick("Enter number of picks:  ")
 
+    for i in range(quick_picks_count):
+        quick_pick = generate_quick_pick()
+        print(" ".join(f"{number:2}" for number in quick_pick))
+
 def get_valid_pick(prompt):
     """Keeps validating and returns value only when a valid integer is entered"""
     while True:
@@ -18,5 +22,16 @@ def get_valid_pick(prompt):
                 print("Please enter a number more than 0.")
         except ValueError:
             print("Invalid input. Try again.")
+
+def generate_quick_pick():
+    """Generate a single row of random numbers"""
+    quick_pick = []
+
+    while len(quick_pick) < NUMBERS_PER_LINE:
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        if number not in quick_pick:
+            quick_pick.append(number)
+    quick_pick.sort()
+    return quick_pick
 
 main()
