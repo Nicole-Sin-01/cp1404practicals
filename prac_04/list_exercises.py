be2fe2af1e073679570f1d445a9573a1f9ba6c80
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-23 10:11:02 +0800

    Added a simple username checker based on a preexisting list of names

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -45,3 +45,14 @@ def print_results(numbers):
     print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
 
 main()
+
+#2. Woefully inadequate security checker
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
