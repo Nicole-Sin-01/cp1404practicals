e57d6f6651c23e0c6a6da04fb31f079fccf6abc9
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-02-24 17:06:42 +0800

    Added dictionary of various colours and their codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical
+Hexadecimal Colour Code Lookup
+Allows the user to look up hexadecimal codes for common color names.
+"""
+
+# Dictionary of color names and their corresponding hex codes
+COLOR_TO_HEX = {"Amethyst": "#9966cc", "DarkOrchid": "#9932cc", "Heliotrope": "#df73ff", "Lavender": "#e6e6fa",
+                "Lilac": "#c8a2c8", "Mauve": "#e0b0ff", "Orchid": "#da70d6", "Periwinkle": "#ccccff", "Plum": "#dda0dd",
+                "Thistle": "#d8bfd8"}
+
+print("Hexadecimal Colour Code Lookup")
+print("Please enter a color name to get its hex code.")
