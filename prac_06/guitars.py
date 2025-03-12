e5dc04942a9fa251cfd7c689d24b5a88384c4230
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-03-12 16:28:42 +0800

    Initial commit

---
+++ prac_06/guitars.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+Guitar Collection Program
+Estimate: 10 minutes
+Actual:
+
+Start the program and print a greeting.
+Ask the user for guitar details (name, year, cost) in a loop until they enter a blank name.
+Store each guitar as an object in a list.
+After input is complete, print a list of all stored guitars.
+Check if each guitar is vintage (50 years or older) and display it accordingly.
+End the program.
+"""
+
+from prac_06.guitar import Guitar
+
