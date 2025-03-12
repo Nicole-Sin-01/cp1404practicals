76c6e9ff089b76040d1025361fbf43b65b5918cd
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-03-12 16:19:00 +0800

    Added init and str representation

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,9 +1,20 @@
 """
 CP1404/CP5632 Practical
 Guitar Class
-Estimate: 10 minutes
+Estimate: 7 minutes
 Actual:
 """
 
 class Guitar:
     """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
