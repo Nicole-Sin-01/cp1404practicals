74fa2a13bf25f7cdc21ac1429a452d623e27f5a6
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-03-12 16:07:34 +0800

    Added str representation for Programming Language class and added is_dynamic method to check if the language is dynamically typed

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,3 +14,11 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Return a string representation of a ProgrammingLanguage object."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Return True if the language is dynamically typed, False otherwise."""
+        return self.typing == "Dynamic"
