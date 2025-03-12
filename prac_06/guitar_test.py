eaad421adc84806c58f98d4c3647862cb1cc1c28
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-03-12 16:24:48 +0800

    Added test values and matched expectations

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,4 +3,20 @@ CP1404/CP5632 Practical
 Guitar Class Testing
 Estimate: 5 minutes
 Actual:
+
 """
+
+from prac_06.guitar import Guitar
+
+def main():
+    """Test Guitar class methods."""
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013, 500)
+
+    print(f"{gibson.name} get_age() - Expected 103. Got {gibson.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 12. Got {another_guitar.get_age()}")
+
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
+
+main()
