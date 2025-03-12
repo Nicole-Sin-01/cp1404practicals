80cd0e97bcb25feb97e9b4a78dae895ba42cdf9a
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2025-03-12 15:57:27 +0800

    Initial Commit

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,19 +6,13 @@ This is why we name our folders with no spaces or capitals, as valid module name
 
 from prac_06.car import Car
 
+
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(name = "My car", fuel = 180)
+    my_car = Car(180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
-    """Limo car object"""
-    limo = Car(name = "Limo", fuel = 100) # initialised with 100 units of fuel
-    limo.add_fuel(20) # add 20 more units of fuel
-    print(f"Limo has fuel: {limo.fuel}")
-
-    limo.drive(115) # drive the car 115 km
-    print(limo)
 
 main()
